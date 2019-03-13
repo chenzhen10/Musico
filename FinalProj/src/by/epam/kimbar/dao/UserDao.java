816732b78968308ba2a50257530c789c9065555a
chenzhen10 <teekeen32@gmail.com>
2019-03-13 12:18:53 +0300

    Revert "Updated version may contain some bugs"
    This reverts commit 14c07f5d7ec8160fab3830c3bf512b45098a985c.

--- FinalProj/src/by/epam/kimbar/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package by.epam.kimbar.dao;
-
-import by.epam.kimbar.dao.exception.DaoException;
-import by.epam.kimbar.entity.User;
-
-public interface UserDao {
-    User authentication(String login, String password) throws DaoException;
-    boolean registration(String login, String password, String username) throws DaoException;
-}
--- FinalProj/src/by/epam/kimbar/entity/Tag.java
+++
@@ -1,7 +0,0 @@
-package by.epam.kimbar.entity;
-
-
-
-public enum  Tag  {
-    HipHop,Jazz,Blues,Folk,Metal,Comedy ,Country, Latin
-}
